27f90715e603a158a683b65bc237caa348762422
elton jefferson <eltonjncorreia@gmail.com>
2018-05-30 11:02:05 -0300

    modelo para receber mensagens

--- landingpage/core/models.py
+++ landingpage/core/models.py
@@ -1,3 +1,9 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+class Contato(models.Model):
+    nome = models.CharField('Nome', max_length=255)
+    email = models.EmailField('Email', max_length=255)
+    telefone = models.CharField('Telefone', max_length=11, validators=[RegexValidator(r'^(\d+)$')])
+    mensagem = models.TextField()
